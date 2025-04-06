8e90abaf485ddfd61ba2a5c4f988fdc500f686bb
Nachom3 <ignacio.mastran@gmail.com>
2025-04-06 19:19:02 -0300

    asd

---
+++ Binario/complemento2.c
@@ -0,0 +1,24 @@
+// Complemento a 2
+//escribir un programa en C que muestre en pantalla la representación binaria del complemento a dos del entero ingresado por el usuario.
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+    unsigned mask = 1 << 31;
+
+    int num;
+    printf("Ingrese un numero entero: ");
+    scanf("%d", &num);
+
+    while(mask > 0){
+        if(num & mask){
+            printf("1");
+        }else{
+            printf("0");
+        }
+        mask = mask >> 1;
+    }
+    return 0;
+}
