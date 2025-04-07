1e3d2d8d75c75bcd00decccedab3870a28bb3fc6
Nachom3 <ignacio.mastran@gmail.com>
2025-04-07 12:42:39 -0300

    EJ3 TERMINADO

--- Binario/ejercicio3.c
+++ Binario/ejercicio3.c
@@ -7,43 +7,54 @@
 
 unsigned int cambiar(unsigned int numero);
 
+void checkLevels(unsigned int numero);
+
 int main (){
 
     unsigned int numero;
     printf("Ingrese el entero que corresponde: \n");
     scanf("%u", &numero);
     unsigned int mask = 15;
-    for (int i = 0; i < 8; i++)
-    {
-        printf("La bomba numero %d se encuentra en el nivel: %u \n", i, mask & numero);
-        numero >> 4;
-    }
-    
-    cambiar(numero);
-    
+    checkLevels(numero);
+    numero = cambiar(numero);
+    printf("-----------------------------------------------\n");
+    checkLevels(numero);
     return 0;
 }
 
 unsigned int cambiar(unsigned int numero){
     int eleccion; 
-    unsigned int valor;
+    int valor;
     printf("Elija un numero entre 0 y 7, siendo este el numero de bomba, o ponga 100 si desea cambiar todas ");
     scanf("%d", &eleccion);
 
     printf("Elija el valor a cambiar la/las bombas(entre 0 y 15)");
-    scanf("%u", &valor);
+    scanf("%d", &valor);
 
-    unsigned int data;
     if (eleccion == 100){
+        numero = 0;
         for (int i = 0; i < 8; i++)
         {
-            data += valor << (i*4); 
-            printf("Bomba numero %d cambiada a %u valor \n", i,valor);
+            numero += valor << (i*4);
+            printf("Bomba numero %d cambiada a %d valor \n", i,valor);
         }
- 
-    }else{
-        
+    }else if(eleccion >= 0 && eleccion <= 7){
+        unsigned int mask = ~(15 << eleccion*4);
+        numero = numero & mask; //cambio a 0
+        unsigned int valormask = valor << eleccion*4;
+        numero = numero | valormask; // cambio al valor
+        printf("Bomba numero %d cambiada a %d\n", eleccion, valor);
     }
     
+    return numero;
+}
 
+void checkLevels(unsigned int numero){
+    unsigned int mask = 15;
+    for (int i = 0; i < 8; i++)
+    {
+        printf("La bomba numero %d se encuentra en el nivel: %u \n", i, mask & numero);
+        numero = numero >> 4;
+    }
+    
 }
