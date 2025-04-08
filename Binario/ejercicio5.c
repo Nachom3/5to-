c80b4fb027406f87a520be2cbcb63f7a16e17b1f
Nachom3 <ignacio.mastran@gmail.com>
2025-04-08 16:07:00 -0300

    5 semihecho

---
+++ Binario/ejercicio5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mostrarPermisos(unsigned int numero);
+
+int main (){
+    unsigned int numero;
+
+    printf("Ingrese el permiso: ");
+    scanf("%d", &numero);
+
+    while(numero < 64){
+        printf("Permisos incorrectos, ingrese uno válido");
+        scanf("%d", &numero);
+    }
+
+    mostrarPermisos(numero);
+
+    return 0;
+}
+
+void mostrarPermisos(int unsigned numero){
+    if(numero & 128 && numero & 64){
+        printf("Es para ambos generos ");
+    }else if(numero & 128){
+        printf("Es para hombres ");
+    }else{
+        printf("Es para mujeres ");
+    }
+    int mask,año=0;
+    mask = 1 << año;
+    while(año < 6){
+        if(mask && numero){
+            printf(", de %d ano", año+1);
+        }
+        año ++;
+        mask = 0;
+        mask = 1 << año;
+    }
+
+}
