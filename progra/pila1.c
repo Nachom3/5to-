f71016528d7edbf0cc8cf4638d13d9ca72844a6b
Nachom3 <ignacio.mastran@gmail.com>
2025-05-23 12:55:24 -0300

    towerdef

---
+++ progra/pila1.c
@@ -0,0 +1,22 @@
+//1. Considera la siguiente estructura
+//typedef struct _Pila {
+//int datos[MAX_PILA];
+//int ultimo;
+//} Pila;
+//y defini la interfaz de una Pila segun la definicion vista en clase. Se sugiere utilizar los siguientes prototipospara las operaciones.
+//  Pila *pila crear(); inicializa una pila vacıa.
+//  int pila es vacia(Pila*); determina si la pila est´a vac´ıa.
+//  int pila tope(Pila*); imprime el elemento en el tope de la pila.
+//  void pila apilar(Pila*, int); agrega un elemento a la pila.
+//  void pila desapilar(Pila*); elimina un elemento de la pila.
+//  void pila imprimir(Pila*); imprime todos los elementos de la pila.
+//  void pila destruir(Pila*); libera la memoria pedida para la pila.
+
+#include <librerias.h>
+
+    
+
+int main(){
+
+    return 0;
+}
